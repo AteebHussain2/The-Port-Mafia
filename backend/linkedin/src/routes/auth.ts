3db60e74a8b3f1016e9e08e05a2576a3f0c38ef7
AteebHussain2 <student2ateeb.hussain@gmail.com>
2026-07-14 20:54:50 +0500

    feat: linkedin integration, storing profile data and necessary encrypted tokens.

---
+++ backend/linkedin/src/routes/auth.ts
@@ -0,0 +1,21 @@
+import { Elysia, t } from 'elysia';
+import { tokenExchange } from '../actions/auth/token-exchange';
+
+export const authRoutes = new Elysia({ prefix: '/auth' })
+    .post("/exchange", async ({ body, status, headers }) => {
+        const userId = headers["x-user-id"];
+
+        const { code, redirect_uri } = body as { code: string; redirect_uri: string };
+
+        if (!code || !redirect_uri) {
+            return status(400, { error: "Missing authorization code or redirect URI" });
+        }
+
+        const res = await tokenExchange(code, redirect_uri, userId);
+
+        return status(res.status, res.message)
+    }, {
+        headers: t.Object({
+            'x-user-id': t.String({ error: "Unauthorized: Missing API Gateway User ID" })
+        })
+    });
