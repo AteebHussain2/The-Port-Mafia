ebe13bf212fffb39a4cbf5578ddc6f989ad7fbc2
AteebHussain2 <student2ateeb.hussain@gmail.com>
2026-07-16 22:56:33 +0500

    fix: fixed db migrations error (Hopefully)

--- backend/main/prisma/migrations/20260716174445_main_init/migration.sql
+++ backend/main/prisma/migrations/20260716174445_main_init/migration.sql
@@ -1,3 +1,9 @@
+-- CreateEnum
+CREATE TYPE "APPTYPE" AS ENUM ('LINKEDIN', 'X', 'REDDIT', 'FACEBOOK', 'INSTAGRAM', 'THREADS', 'TIKTOK');
+
+-- CreateEnum
+CREATE TYPE "APPSTATUS" AS ENUM ('CONNECTED', 'DISCONNECTED');
+
 -- CreateTable
 CREATE TABLE "Session" (
     "id" TEXT NOT NULL,
@@ -23,8 +29,23 @@ CREATE TABLE "RefreshToken" (
     CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "connectedApps" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "app" "APPTYPE" NOT NULL,
+    "status" "APPSTATUS" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "connectedApps_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "RefreshToken_hash_key" ON "RefreshToken"("hash");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "connectedApps_userId_app_key" ON "connectedApps"("userId", "app");
 
 -- AddForeignKey
 ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_sessionId_fkey" FOREIGN KEY ("sessionId") REFERENCES "Session"("id") ON DELETE CASCADE ON UPDATE CASCADE;
